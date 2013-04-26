1fc4b1a1851e50ea8af4b9036326e373f3cb3fdf
jaecheol <jaecheol@captv.co.kr>
2013-04-26 13:41:09 +0900

    Usage : SampleLog.exe Ratio Filename

---
+++ SampleLog/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace SampleLog
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int sampleRate = int.Parse(args[0]);
+            string fileName = args[1];
+
+            Trace.WriteLine(sampleRate);
+            Trace.WriteLine(fileName);
+
+            foreach (var matchedLine in File.ReadLines(fileName)
+                                            .Select((line, lineNo) => new {Line = line, LineNo = lineNo})
+                                            .Where(lineInfo =>
+                                                   lineInfo.Line.StartsWith("#") || lineInfo.LineNo%sampleRate == 0))
+            {
+                Console.WriteLine(matchedLine.Line);
+            }
+        }
+    }
+}
